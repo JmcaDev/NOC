5affce101e2ae16bbd17b7dce134bde0c7986c4a
JmcaDev <jmcavzla@gmail.com>
2025-07-05 19:24:01 -0400

    MongoDB

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,31 @@
 import { Server } from "../src/presentation/server"
 import { envs } from "./config/plugins/envs.plugin"
+import { LogModel, MongoDatabase } from "./data/mongoDB"
 
 
-function main(){
-    Server.start()
-    // console.log(envs)
+async function main(){
+
+    await MongoDatabase.connect({
+        mongoUrl: envs.MONGO_URL,
+        dbName: envs.MONGO_DB_NAME
+    })
+
+    //Crear una coleccion = table, documento = registro
+    // const newLog = await LogModel.create({
+    //     message: "Test message desde Mongo",
+    //     origin: "app.ts",
+    //     level: "low",
+    // })
+
+    // await newLog.save()
+
+    // console.log(newLog)
+
+    const logs = await LogModel.find()
+    console.log(logs[2].message)
+
+
+    // Server.start()
 }
 
 (async() => {
